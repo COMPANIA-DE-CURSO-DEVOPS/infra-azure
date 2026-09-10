378bf60e8f21a1c2a89e8fa7ab01fa52606226ea
FioRuiz <fruiz4827@gmail.com>
2026-09-09 22:42:42 -0500

    UPDATE  de apellido

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-cicd-terraform-app-fruizc" # Reemplazar por apellido
-    storage_account_name = "tfstatefruizc"                # Reemplazar por apellido
+    storage_account_name = "tfstatefruizc03"              # Cuenta existente en Azure
     container_name       = "tfstate"
     key                  = "dev/terraform.tfstate"
   }
--- envs/prd/backend.tf
+++ envs/prd/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = ""rg-cicd-terraform-app-fruizc" # Reemplazar por apellido
-    storage_account_name = "tfstatefruizc"                # Reemplazar por apellido
+    resource_group_name  = "rg-cicd-terraform-app-fruizc"
+    storage_account_name = "tfstatefruizc03"
     container_name       = "tfstate"
     key                  = "prd/terraform.tfstate"
   }
